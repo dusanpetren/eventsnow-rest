d39a11458bf4aa60815906824e971634ccf259e0
dusannnn <dusanpetren@outlook.com>
2020-01-08 10:04:50 +0100

    Add db connection properties

--- src/main/java/vse/cz/vseblog/configuration/SecurityConfiguration.java
+++ src/main/java/vse/cz/vseblog/configuration/SecurityConfiguration.java
@@ -16,7 +16,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
 import vse.cz.vseblog.repository.UserRepository;
 
 /**
@@ -71,8 +76,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				// store user's state.
 						exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);        // Add a filter to validate the tokens with every request
+		httpSecurity.cors();
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("*"));
+		configuration.setAllowedMethods(Arrays.asList("HEAD",
+				"GET", "POST", "PUT", "DELETE", "PATCH"));
+		// setAllowCredentials(true) is important, otherwise:
+		// The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+		configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
 
--- src/main/java/vse/cz/vseblog/configuration/WebConfiguration.java
+++ src/main/java/vse/cz/vseblog/configuration/WebConfiguration.java
@@ -2,23 +2,17 @@ package vse.cz.vseblog.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author dusan.petren
  */
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
-
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("https://eventsnow-rest.herokuapp.com/", "https://pure-sands-41229.herokuapp.com/");
-
+				.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 	}
-
-
 }
